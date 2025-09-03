8756d67aa8df519a9c123b8eddea782361e3b665
thiago619 <jurassicpark171@gmail.com>
2025-09-03 13:32:47 -0300

    validação ao criar o produto. Não aceita produtos com valor menor que 1

--- src/main/java/me/dio/restaurante/domain/model/Request/CreateProdutoRequest.java
+++ src/main/java/me/dio/restaurante/domain/model/Request/CreateProdutoRequest.java
@@ -2,6 +2,8 @@ package me.dio.restaurante.domain.model.Request;
 
 import java.math.BigDecimal;
 
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -13,7 +15,10 @@ import me.dio.restaurante.domain.model.Produto;
 @Getter
 @Setter
 public class CreateProdutoRequest {
+    @NotNull(message="descrição é obrigatória")
     private String descricao;
+    @NotNull(message="preço é obrigatório")
+    @Min(value=1)
     private BigDecimal preco;
 
     public Produto toModel(){
